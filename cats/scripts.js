c688c610b8e11e5dfe34f4e61ad0553d48f538f1
Your Name <you@example.com>
2022-07-08 16:06:18 +0100

    adding cat facts

--- cats/scripts.js
+++ cats/scripts.js
@@ -6,25 +6,26 @@ container.setAttribute('class', 'container')
 app.appendChild(container)
 
 var request = new XMLHttpRequest()
-request.open('GET', 'https://dog.ceo/api/breed/spaniel/cocker/images', true)
+request.open('GET', 'https://meowfacts.herokuapp.com/', true)
 request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
   if (request.status >= 200 && request.status < 400) {
-    var images = data.message;
-    images.forEach(image => {
+    var images = data.data;
+    images.forEach(fact => {
       const card = document.createElement('div')
       card.setAttribute('class', 'card')
 
       const h1 = document.createElement('h1')
-      h1.textContent = "Cocker Spaniel";
+      h1.textContent = "Cat Fact";
 
-      const img = document.createElement('img')
-      img.src = `${image}`;
+      const p = document.createElement('p')
+      p.textContent = `${fact.data}...`
+
 
       container.appendChild(card)
       card.appendChild(h1)
-      card.appendChild(img)
+      card.appendChild(p)
     })
   } else {
     const errorMessage = document.createElement('marquee')
