05a79260bafeb552446d546b5b8083d6c898d377
Your Name <you@example.com>
2022-07-08 14:41:03 +0100

    buttons added

---
+++ dogs/scripts.js
@@ -0,0 +1,35 @@
+const app = document.getElementById('root')
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(container)
+
+var request = new XMLHttpRequest()
+request.open('GET', 'https://dog.ceo/api/breeds/image/random', true)
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(image => {
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
+
+      const img = document.createElement('img')
+      img.src = image.message;
+
+      const h1 = document.createElement('h1')
+      h1.textContent = image.message;
+
+      container.appendChild(card)
+      card.appendChild(img)
+      card.appendChild(h1)
+    })
+  } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
+  }
+}
+
+request.send()
