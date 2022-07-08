ac7aa84920d64c5c7a674d6d63340ee51e339fc7
Your Name <you@example.com>
2022-07-08 15:25:08 +0100

    Added dog api

--- dogs/scripts.js
+++ dogs/scripts.js
@@ -6,24 +6,26 @@ container.setAttribute('class', 'container')
 app.appendChild(container)
 
 var request = new XMLHttpRequest()
-request.open('GET', 'https://dog.ceo/api/breed/hound/images/random', true)
+request.open('GET', 'https://dog.ceo/api/breed/spaniel/cocker/images', true)
 request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
+  i = 0
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(image => {
+    var images = data.message;
+    images.forEach(image => {
       const card = document.createElement('div')
       card.setAttribute('class', 'card')
 
-      const img = document.createElement('img')
-      img.src = image.message;
-
       const h1 = document.createElement('h1')
-      h1.textContent = image.message;
+      h1.textContent = "Cocker Spaniel";
+
+      const img = document.createElement('img')
+      img.src = `${image}`;
 
       container.appendChild(card)
-      card.appendChild(img)
       card.appendChild(h1)
+      card.appendChild(img)
     })
   } else {
     const errorMessage = document.createElement('marquee')
